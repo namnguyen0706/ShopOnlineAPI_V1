2035cad047b05ead373ad76433e7f6895a755457
namnguyen0706 <namk45thue@gmail.com>
2019-04-22 17:15:19 +0700

    aaaa

---
+++ ShopOnlineAPI/ShopOnlineAPI.Common/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopOnlineAPI.Common
+{
+    public class Class1
+    {
+        //kdkfsdfsdf
+        //skdhfhdfjghkdfg;
+    }
+}
---
+++ ShopOnlineAPI/ShopOnlineAPI.Common/Class2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopOnlineAPI.Common
+{
+    class Class2
+    {
+        //njkfkgdfkgjksd
+    }
+}
