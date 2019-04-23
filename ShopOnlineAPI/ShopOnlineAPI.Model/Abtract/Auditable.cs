c44b6c0ac2f7da39b1fba1bce709c83fd6f16f4c
namnguyen0706 <namk45thue@gmail.com>
2019-04-23 09:44:22 +0700

    Add models of project

---
+++ ShopOnlineAPI/ShopOnlineAPI.Model/Abtract/Auditable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShopOnlineAPI.Model.Abtract
+{
+    public abstract class Auditable : IAuditable
+    {
+        public DateTime? CreatedDate { set; get; }
+
+        [MaxLength(256)]
+        public string CreatedBy { set; get; }
+
+        public DateTime? UpdatedDate { set; get; }
+
+        [MaxLength(256)]
+        public string UpdatedBy { set; get; }
+
+        [MaxLength(256)]
+        public string MetaKeyword { set; get; }
+
+        [MaxLength(256)]
+        public string MetaDescription { set; get; }
+
+        public bool Status { set; get; }
+    }
+}
---
+++ ShopOnlineAPI/ShopOnlineAPI.Model/Models/Visitorstatistic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShopOnlineAPI.Model.Models
+{
+    [Table("VisitorStatistics")]
+    public class Visitorstatistic
+    {
+        [Key]
+        public Guid ID { set; get; }
+
+        [Required]
+        public DateTime VisitedDate { set; get; }
+
+        [MaxLength(50)]
+        public string IPAddress { set; get; }
+    }
+}
