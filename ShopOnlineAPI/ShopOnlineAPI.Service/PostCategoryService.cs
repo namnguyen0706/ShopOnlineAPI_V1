98679f56e9a4313b8293f80d0a33ff2070af9b6e
namnguyen0706 <namk45thue@gmail.com>
2019-04-26 10:39:48 +0700

    Lesson 10 - Unit Test

---
+++ ShopOnlineAPI/ShopOnlineAPI.Service/PostCategoryService.cs
@@ -0,0 +1,70 @@
+﻿using ShopOnlineAPI.Data.Infrastructure;
+using ShopOnlineAPI.Data.Repositories;
+using ShopOnlineAPI.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopOnlineAPI.Service
+{
+    public interface IPostCategoryService
+    {
+        PostCategory Add(PostCategory postCategory);
+
+        void Update(PostCategory postCategory);
+
+        PostCategory Delete(int id);
+
+        IEnumerable<PostCategory> GetAll();
+
+        IEnumerable<PostCategory> GetAllByParentId(int parentId);
+
+        PostCategory GetById(int id);
+    }
+
+    public class PostCategoryService : IPostCategoryService
+    {
+        IPostCategoryRepository _postCategoryRepository;
+        IUnitOfWork _unitOfWork;
+
+        public PostCategoryService(IPostCategoryRepository postCategoryRepository, IUnitOfWork unitOfWork)
+        {
+            this._postCategoryRepository = postCategoryRepository;
+            this._unitOfWork = unitOfWork;
+        }
+
+        public PostCategory Add(PostCategory postCategory)
+        {
+            return _postCategoryRepository.Add(postCategory);
+        }
+
+        public void Update(PostCategory postCategory)
+        {
+            _postCategoryRepository.Update(postCategory);
+        }
+
+        public PostCategory Delete(int id)
+        {
+            return _postCategoryRepository.Delete(id);
+        }
+
+        public IEnumerable<PostCategory> GetAll()
+        {
+            return _postCategoryRepository.GetAll();
+        }
+
+        public IEnumerable<PostCategory> GetAllByParentId(int parentId)
+        {
+            return _postCategoryRepository.GetMulti(x => x.Status && x.ParentID == parentId);
+        }
+
+        public PostCategory GetById(int id)
+        {
+            return _postCategoryRepository.GetSingleById(id);
+        }
+
+       
+    }
+}
